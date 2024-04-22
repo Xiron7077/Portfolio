66ddbf1b64907647ec6b304023035d6d8550509b
xiron <xiron7077@gmail.com>
2024-04-22 20:11:25 +0500

    Changed home option to logo

---
+++ Frontend/src/index.js
@@ -0,0 +1,11 @@
+gsap.registerPlugin(Flip);
+
+let link_text = document.querySelector(".gik-link").textContent;
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".gik-link").addEventListener(
+        "mouseover",
+        () => {
+        }
+    );
+});     
